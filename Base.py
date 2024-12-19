1aa984a8bbff875b3c5dbcef7c27c80726b892c0
lii4ee <angelolife2002@gmail.com>
2024-12-19 13:59:08 +0530

    wow

--- Base.py
+++ Base.py
@@ -1,13 +1,9 @@
 from dotenv import load_dotenv
 import os
 
-# Load environment variables from the .env file
 dotenv_path = r"D:\Project\Automating Tweeting\apikeys.env"
 load_dotenv(dotenv_path=dotenv_path)
 
-# Access the values of the environment variables
 twitter_api_key = os.getenv("TWITTER_API_KEY")
 
 print("Twitter API Key:", twitter_api_key)
-
-\
