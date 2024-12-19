4b1c4e9cf422337c441fe4baa15d0c59bded358b
lii4ee <angelolife2002@gmail.com>
2024-12-19 12:31:46 +0530

    first commit

--- Base.py
+++ Base.py
@@ -7,8 +7,5 @@ load_dotenv(dotenv_path=dotenv_path)
 
 # Access the values of the environment variables
 twitter_api_key = os.getenv("TWITTER_API_KEY")
-twitter_api_secret_key = os.getenv("TWITTER_API_SECRET_KEY")
-twitter_access_token = os.getenv("TWITTER_ACCESS_TOKEN")
-twitter_access_secret = os.getenv("TWITTER_ACCESS_SECRET")
 
 print("Twitter API Key:", twitter_api_key)
